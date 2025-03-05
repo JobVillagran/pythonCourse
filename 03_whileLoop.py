74e96f31d7e5e6df3dc62fdd1a5a4a77d031fcd7
Job Villagran <jobvillagran@gmail.com>
2025-03-04 21:43:28 -0600

    mod/while_loop_pt2

--- 03_whileLoop.py
+++ 03_whileLoop.py
@@ -3,3 +3,21 @@ it = 4
 while it > 1:
     print(it)
     it = it - 1
+print("While loop")
+
+miau = 4
+# Skipping an specific number
+while miau > 1:
+    if miau != 3:
+        print(miau)
+    miau = miau - 1
+print("While loop with conditional")
+
+et = 4
+# Skipping an specific number + break
+while et > 1:
+    if et == 3:
+        break
+    print(et)
+    et = et - 1
+print("While loop with conditional and break")
