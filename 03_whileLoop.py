0172ddf02fd4c5728f84eb4b706c233d67dc2cc1
Job Villagran <jobvillagran@gmail.com>
2025-03-07 17:07:27 -0600

    mod/while_loops_pt3

--- 03_whileLoop.py
+++ 03_whileLoop.py
@@ -13,11 +13,24 @@ while miau > 1:
     miau = miau - 1
 print("While loop with conditional")
 
-et = 4
+et = 10
 # Skipping an specific number + break
 while et > 1:
     if et == 3:
         break
     print(et)
     et = et - 1
+print("While loop with conditional and break")
+
+at = 10
+# multiple if statemens using continue(skipped specifyc data)
+while at > 1:
+    if at == 9:
+        at = at - 1
+        continue
+
+    if at == 3:
+        break
+    print(at)
+    at = at - 1
 print("While loop with conditional and break")
