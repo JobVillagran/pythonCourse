e01e93fcda805a36764272a5b8c6a9bcb9ae0706
Job Villagran <jobvillagran@gmail.com>
2025-03-07 18:03:31 -0600

    mod/functions_pt1

---
+++ s4_functions.py
@@ -0,0 +1,21 @@
+"""
+This module contains simple functions to print greetings.
+"""
+
+
+# Function declaration
+def greet_me():
+    """Prints a simple greeting."""
+    print("Hello world")
+
+
+greet_me()
+
+
+# Adding parameters
+def hello_world():
+    """Prints 'Hello world'."""
+    print("Hello world")
+
+
+hello_world()
