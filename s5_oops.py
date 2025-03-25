9fdc7a79a61d303eeb68931bb658c3a5d8bdd990
Job Villagran <jobvillagran@gmail.com>
2025-03-25 05:37:09 -0600

    oops_pt2

--- s5_oops.py
+++ s5_oops.py
@@ -1,17 +1,30 @@
+# Self keyword is mandatory for calling variables nambes into method
+# instance and class variables have different purpose
+# constructor name should be _init_
+# When you don't create a constructor, default constructor will be called: def __init__(self):
+# new keyword is not required when you create an object
+
+
 class Calculator:
     num = 100
-    # When you don't create a constructor, default constructor will be called: def __init__(self):
 
     # Constructor always start with init
-    def __init__(self):
-         print("I'm calling auto when object is created with no constructor")
+    def __init__(self, a, b):
+        self.firstNumber = a
+        self.secondNumber = b
+        print("I'm calling auto when object is created with no constructor")
 
     def getData(self):
         print("This is a method inside a Class")
 
+    def Summation(self):
+        return self.firstNumber + self.secondNumber + Calculator.num
 
-obj = Calculator()
+
+obj = Calculator(2, 3)
 obj.getData()
-print(obj.num)
+print(obj.Summation())
 
-
+obj1 = Calculator(4, 5)
+obj1.getData()
+print(obj1.Summation())
