147afe1dbc57c359942a85969a635a6a2dbf5f6f
Job Villagran <jobvillagran@gmail.com>
2025-03-25 07:26:20 -0600

    mod/try_catch

---
+++ s13_try_catch.py
@@ -0,0 +1,21 @@
+ItemInCart = 0
+# 2 items will be added to the cart
+
+if ItemInCart != 2:
+    # raise Exception("Products Cart count not matching")
+    pass
+
+
+assert ItemInCart == 0
+
+try:
+    with open("filelog.txt", "r") as reader:
+        reader.read()
+except:
+    print("Somehow I reached this block because there is a failure " "in the try block")
+
+try:
+    with open("filelog.txt", "r") as reader:
+        reader.read()
+except Exception as e:
+    print(e)
