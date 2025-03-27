9da78ba4ddaefceffc7bea3c16db7223f4df3b85
Job Villagran <jobvillagran@gmail.com>
2025-03-26 22:34:46 -0600

    mod/locators_pt2

--- s16_locators.py
+++ s16_locators.py
@@ -13,11 +13,18 @@ time.sleep(3)
 driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Larry King")
 driver.find_element(By.NAME, "email").send_keys("hello@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("1234")
+time.sleep(3)
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
+time.sleep(3)
 driver.find_element(By.ID, "exampleCheck1").click()
 driver.find_element(By.XPATH, "//input[@value='Submit']").click()
 message = driver.find_element(By.CLASS_NAME, "alert-success").text
 print(message)
 assert "Success" in message
+
+
+driver.find_element(By.XPATH, "(//input[@type='text'])[3]").send_keys("helloagain")
+driver.find_element(By.XPATH, "(//input[@type='text'])[3]").clear()
 
 print(driver.title)
 time.sleep(5)
