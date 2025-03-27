1f35ad15a2fdb863bd14c79b26ca1878eb50f02a
Job Villagran <jobvillagran@gmail.com>
2025-03-26 21:41:39 -0600

    mod/locators

--- s16_locators.py
+++ s16_locators.py
@@ -10,9 +10,14 @@ driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 time.sleep(3)
 
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Larry King")
 driver.find_element(By.NAME, "email").send_keys("hello@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("1234")
 driver.find_element(By.ID, "exampleCheck1").click()
+driver.find_element(By.XPATH, "//input[@value='Submit']").click()
+message = driver.find_element(By.CLASS_NAME, "alert-success").text
+print(message)
+assert "Success" in message
 
 print(driver.title)
 time.sleep(5)
