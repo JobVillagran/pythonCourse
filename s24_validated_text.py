f1b15ec337a957e556155176bec79110c22086c1
Job Villagran <jobvillagran@gmail.com>
2025-07-05 15:29:52 -0600

    mod/section_13

---
+++ s24_validated_text.py
@@ -0,0 +1,60 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/seleniumPractise/")
+
+driver.implicitly_wait(2)
+
+expectedList = ['Cucumber - 1 Kg', 'Raspberry - 1/4 Kg', 'Strawberry - 1/4 Kg']
+actualList = []
+
+driver.find_element(By.CLASS_NAME, "search-keyword").send_keys("ber")
+time.sleep(2)
+
+results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
+count = len(results)
+assert count > 0
+for result in results:
+    # We checked if the text of the element is in the expected list w/ APPEND
+    actualList.append(result.find_element(By.XPATH, "h4").text)
+    result.find_element(By.XPATH, "div/button").click()
+
+assert expectedList == actualList
+
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+
+
+driver.find_element(By.CLASS_NAME, "promoCode").send_keys("rahulshettyacademy")
+driver.find_element(By.CLASS_NAME, "promoBtn").click()
+wait = WebDriverWait(driver, 10)
+
+wait.until(EC.presence_of_element_located((By.CLASS_NAME, "promoInfo")))
+print(driver.find_element(By.CLASS_NAME, "promoInfo").text)
+time.sleep(2)
+
+prices = driver.find_elements(By.CSS_SELECTOR, "tr td:nth-child(5) p")
+sum = 0
+for price in prices:
+    # Here we convert the text to an integer
+    sum = sum + int(price.text)
+
+print(sum)
+#  Here we convert the text to an integer
+totalAmount = int(driver.find_element(By.CLASS_NAME, "totAmt").text)
+assert sum == totalAmount
+
+totalAfterDiscount = float(driver.find_element(By.CLASS_NAME, "discountAmt").text)
+print(totalAfterDiscount)
+assert totalAmount > totalAfterDiscount
+
+
+time.sleep(3)
